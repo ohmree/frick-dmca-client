f87a57b76b907b10fe9d08fcf21667e61fd45bfb
OhmRee <13455401+ohmree@users.noreply.github.com>
2020-12-09 16:50:41 +0200

    Rehauled build tooling and fixed version numbers

--- wasm/rollup.config.js
+++ wasm/rollup.config.js
@@ -6,34 +6,60 @@ import styles from "rollup-plugin-styles";
 
 // `yarn build` -> `production` is true
 // `yarn dev` -> `production` is false
-const production = !process.env.ROLLUP_WATCH;
+const production = !("ROLLUP_WATCH" in process.env);
+// Should we build a single fat HTML for desktop app distribution or a regular web app for serving to a browser?
+const buildSinglePage = "BUILD_SINGLE_PAGE" in process.env;
 
 export default {
     input: {
-        wasm: "Cargo.toml"
+        wasm: "Cargo.toml",
+        css: "styles/app.css"
     },
     output: {
         dir: "dist",
         format: "es",
-        // sourcemap: !production,
+        sourcemap: !production,
+        // assetFileNames: "[name]-[hash][extname]"
     },
     watch: {
-        include: ["src/**/*.rs", "Cargo.toml", "**/*.css"]
+        include: ["src/**/*.rs", "Cargo.toml", "styles/**/*.css"]
     },
     plugins: [
         html({
+            title: "FrickDMCA",
             meta: [
                 {
                     "http-equiv": "Content-Security-Policy",
-                    content: "default-src 'self' inline https://invidious.tube; media-src https://*.googlevideo.com;"
+                    // TODO: maybe find a more secure way to handle this? Perhaps generate a nonce/hash at compile/bundle time?
+                    content: buildSinglePage
+                        && "default-src 'self' https://invidious.kavin.rocks; media-src https://*.googlevideo.com; script-src 'unsafe-inline'; style-src 'unsafe-inline';"
+                        || "default-src 'self' https://invidious.kavin.rocks; media-src https://*.googlevideo.com; style-src 'unsafe-inline'"
+                    // TODO: remove the unsafe style loading when we find a better solution
+                },
+                {
+                    charset:"utf-8"
+                },
+                {
+
+                    "http-equiv":"x-ua-compatible",
+                    content:"ie=edge"
+                },
+                {
+
+                    name:"viewport",
+                    content:"width=device-width, initial-scale=1"
                 }
             ]
         }),
         rust({
-            inlineWasm: true,
+            inlineWasm: buildSinglePage,
             cargoArgs: production && [] || ["--features", "debug"]
         }),
-        styles(),
+        styles({
+            mode: buildSinglePage && "inject" || "inject",
+            include: ["styles/**/*.css"],
+            minimize: production,
+        }),
         production && terser(), // minify, but only in production
         !production && serve("dist")
     ]
